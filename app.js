d6f116334da3fce46dcfe61cce2c0dc910e10879
Athanogs <thanpaez@gmail.com>
2025-08-24 22:36:38 -0600

    Creada funcion agregarAmigo()

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Creación de variables
+let amigos=[];
+
+//Creacion de funciones
+//Funcion para capturar el nombre ingresado por el usuario
+function agregarAmigo(){
+    let nombreAmigo=document.getElementById('amigo').value;
+    if (nombreAmigo.trim()===''){
+        //En caso de haberse dejado en blanco
+        alert('Por favor, inserte un nombre.');
+    }else{
+        //En caso de si haber ingresado un nombre
+        amigos.push(nombreAmigo);
+    }
+    //Para borrar el texto que se escribio
+    document.getElementById('amigo').value='';
+    actualizarLista();
+}
